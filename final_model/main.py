dd80e5029e8e999b44fae9a82387fe2335a8b8fa
Lou Vaughn <ljvaughn0841@gmail.com>
2026-04-16 15:06:15 -0400

    kmeans stuff

--- final_model/main.py
+++ final_model/main.py
@@ -1,5 +1,65 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import pandas as pd
+
+
+
+
+
 def main():
     print("Hello from final-model!")
+
+    # Numbe of requests for each script (how utilized is this script on the web globally?)
+    exposure = pd.read_csv("../exposure_research/output/exposure_filtered_results.csv")
+
+    exposure = exposure.rename(columns={"count": "exposure"})
+
+    exposure['script'] = exposure['script'].str.lower()
+
+    exposure = exposure[["script", "exposure"]]
+    
+    print(exposure.head())
+
+
+
+
+
+    # Number of distinct fonts supporting each script
+    # TODO: CHANGE THIS OUT LATER FOR UPDATED METRICS THAT USE GLOBAL WEB DATA TO SUPPORT DISTINCT FONT COUNT
+    support = pd.read_csv("../support_research/output/google_support_filtered.csv")
+
+
+    support = support.rename(columns={"count": "support"})
+    support = support[["script", "support"]]
+    support['script'] = support['script'].str.lower()
+
+    print(support.head())
+
+    # Since this is using google data we need to rename some things to match the other data we have
+    # It's not perfect yet though could improve upon this further
+    
+    support['script'] = support['script'].str.replace(["chinese-simplified", "chinese-traditional"], "han")
+    support['script'] = support['script'].str.replace("japanese", "katakana")
+
+
+    # TODO: Currently this is backwards deal with it later abs(1 - diversity_index) or something like that 
+    similarity = pd.read_csv("../similarity_research/diversity_research/vit100_outputs/diversity_index_summary.csv")
+
+    similarity = similarity.rename(columns={"diversity_index": "similarity"})
+    similarity = similarity[["script", "similarity"]]
+    similarity['script'] = similarity['script'].str.lower()
+
+
+    print(similarity.head())
+
+    # merge only keeping the main indexes
+    final_df = exposure[["script", "exposure"]].merge(support[["script", "support"]], on="script", how="outer").merge(similarity[["script", "similarity"]], on="script", how="outer")
+
+    print(final_df.head())
+
+
 
 
 if __name__ == "__main__":
